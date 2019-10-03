b9ddd8d078be45de4bffc321190eeecabcf04691
lyubomir87 <52287972+lyubomir87@users.noreply.github.com>
2019-10-03 15:20:02 +0300

    Create Print Diagonals Square Matrix.java

---
+++ Multidimensional Arrays/Print Diagonals Square Matrix.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class diag {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            matrix[i] = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+        int row = 0;
+        int col = 0;
+
+        while (row < size && col < size) {
+            int element = matrix[row][col];
+            System.out.print(element + " ");
+
+
+            row++;
+            col++;
+        }
+        row = size - 1;
+        col = 0;
+        System.out.println();
+        while (row >= 0 && col < size) {
+            int element = matrix[row][col];
+            System.out.print(element + " ");
+
+            row--;
+            col++;
+        }
+    }
+}
